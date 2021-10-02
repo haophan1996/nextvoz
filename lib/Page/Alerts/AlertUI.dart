7a10632b0e8986cbaf659e8d27d48d05b136e58f
chobay <phanh5@wit.edu>
2021-10-02 01:49:53 -0400

    fix alert ui

--- lib/Page/Alerts/AlertUI.dart
+++ lib/Page/Alerts/AlertUI.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:the_next_voz/utils/color.dart';
 import '/Routes/pages.dart';
 import '/GlobalController.dart';
 import '/Page/reuseWidget.dart';
@@ -15,89 +16,123 @@ class AlertsUI extends GetView<AlertsController> {
       body: GetBuilder<AlertsController>(
         id: 'loadingState',
         builder: (controller) {
-          return controller.data['loadingStatus'] == 'loading' ? loading() : ListView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: GlobalController.i.alertList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                decoration: BoxDecoration(
-                    color: GlobalController.i.alertList.elementAt(index)['unread'] == 'true' ? Get.theme.canvasColor : Colors.transparent,
-                    border: Border(
-                      bottom: BorderSide(width: 0.5, color: Theme.of(context).primaryColor),
-                    )),
-                child: itemList(index),
-              );
-            },
-          );
+          return controller.data['loadingStatus'] == 'loading' ? loading() : alertList();
         },
       ),
     );
-
   }
-  Widget loading() => Stack(
-    children: [
-      GetBuilder<AlertsController>(
-          id: 'download',
-          builder: (controller) {
-            return LinearProgressIndicator(
-              value: controller.percentDownload,
-              valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF0CF301)),
-              backgroundColor: Get.theme.backgroundColor,
-            );
-          }),
-    ],
-  );
 
-  Widget itemList(int index) {
-    return CupertinoButton(
-      alignment: Alignment.centerLeft,
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(text: GlobalController.i.alertList.elementAt(index)['username'], style: TextStyle(color: Colors.blue,fontSize: Get.textTheme.bodyText1!.fontSize,)),
-            TextSpan(text: GlobalController.i.alertList.elementAt(index)['status'], style: TextStyle(color: Get.theme.primaryColor,fontSize: Get.textTheme.bodyText1!.fontSize,)),
-            customTitleChild(FontWeight.normal, Colors.blue, GlobalController.i.alertList.elementAt(index)['prefix'] ?? '',
-                GlobalController.i.alertList.elementAt(index)['threadName'], null),
-            GlobalController.i.alertList.elementAt(index)['reaction'] == ''
-                ? TextSpan()
-                : TextSpan(
-                text: 'with ${GlobalController.i.alertList.elementAt(index)['reaction'] == '1' ? 'Ưng ' : 'Gạch '}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: Get.textTheme.bodyText1!.fontSize,
-                    color: GlobalController.i.alertList.elementAt(index)['reaction'] == '1' ? Colors.pinkAccent : Colors.red)),
-            GlobalController.i.alertList.elementAt(index)['reaction'] == ''
-                ? TextSpan()
-                : WidgetSpan(
-              child: Image.asset(
-                'assets/reaction/${GlobalController.i.alertList.elementAt(index)['reaction']}.png',
-                width: 18,
+  Widget alertList()=> ListView.builder(
+      itemCount: GlobalController.i.alertList.length,
+      itemBuilder: (context, index) {
+        return CupertinoButton(child: Container(
+          decoration: BoxDecoration(
+              color: GlobalController.i.alertList.elementAt(index)['unread'] == 'true' ? Get.theme.canvasColor : Colors.transparent,
+              border: Border(
+                bottom: BorderSide(width: 0.5, color: Theme.of(context).primaryColor),
+              )),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 5, right: 5),
+                child: displayAvatar(
+                    35,
+                    GlobalController.i.alertList.elementAt(index)['avatarColor1'],
+                    GlobalController.i.alertList.elementAt(index)['avatarColor2'],
+                    GlobalController.i.alertList.elementAt(index)['username'],
+                    GlobalController.i.alertList.elementAt(index)['avatarLink']),
               ),
-            ),
-            TextSpan(text: '\n' + GlobalController.i.alertList.elementAt(index)['time'], style: TextStyle(color: Get.theme.secondaryHeaderColor))
-          ],
-        ),
-      ),
-      onPressed: () {
-        if (GlobalController.i.alertList.elementAt(index)['unread'] == 'true') {
-          GlobalController.i.alertList.elementAt(index)['unread'] = 'false';
-          controller.update(['loadingState']);
-        }
-        if (GlobalController.i.alertList.elementAt(index)['link'].contains('/u/', 0) == true) {
-          Get.toNamed(Routes.Profile, arguments: [GlobalController.i.alertList.elementAt(index)['link']], preventDuplicates: false);
-        } else if (GlobalController.i.alertList.elementAt(index)['link'].contains('/profile-posts/', 0) == true) {
-          // Go to status
-          print('status');
-        } else {
-          Get.toNamed(Routes.View, arguments: [
-            GlobalController.i.alertList.elementAt(index)['threadName'],
-            GlobalController.i.alertList.elementAt(index)['link'],
-            GlobalController.i.alertList.elementAt(index)['prefix'],
-            GlobalController.i.alertList.elementAt(index)['link'].toString().contains('conversations/messages') ? 1 : 0
-          ]);
-        }
-      },
-    );
-  }
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['username'],
+                          style: TextStyle(
+                              fontFamily: 'BeVietNam', fontSize: Theme.of(context).textTheme.bodyText1!.fontSize, color: Colors.blue)),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['phase1'],
+                          style: TextStyle(fontFamily: 'BeVietNam', fontSize: Theme.of(context).textTheme.bodyText1!.fontSize)),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['title2'],
+                          style: TextStyle(
+                              fontFamily: 'BeVietNam', fontSize: Theme.of(context).textTheme.bodyText1!.fontSize, color: Colors.blue)),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['phase'],
+                          style: TextStyle(
+                              fontFamily: 'BeVietNam', fontSize: Theme.of(context).textTheme.bodyText1!.fontSize)),
+                      GlobalController.i.alertList.elementAt(index)['prefixTitle'] != ''
+                          ? WidgetSpan(
+                        child: Container(
+                          padding: EdgeInsets.only(left: 2, right: 2),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(6)),
+                              color: mapColor[GlobalController.i.alertList.elementAt(index)['prefixTitle']]),
+                          child: Text(
+                            GlobalController.i.alertList.elementAt(index)['prefixTitle'],
+                            style: TextStyle(
+                                fontFamily: 'BeVietNam',
+                                fontSize: Theme.of(context).textTheme.bodyText1!.fontSize,
+                                color: getColorInvert(GlobalController.i.alertList.elementAt(index)['prefixTitle'])),
+                          ),
+                        ),
+                      )
+                          : TextSpan(),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['title'],
+                          style: TextStyle(
+                              fontFamily: 'BeVietNam', fontSize: Theme.of(context).textTheme.bodyText1!.fontSize, color: Colors.blue)),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['phase2'],
+                          style: TextStyle(fontSize: Theme.of(context).textTheme.bodyText1!.fontSize, fontFamily: 'BeVietNam')),
+                      GlobalController.i.alertList.elementAt(index)['hasReact'] == true
+                          ? WidgetSpan(
+                          child: Image.asset(
+                            'assets/reaction/${GlobalController.i.alertList.elementAt(0)['react']}',
+                            width: Theme.of(context).textTheme.headline6!.fontSize,
+                          ))
+                          : TextSpan(),
+                      TextSpan(
+                          text: GlobalController.i.alertList.elementAt(index)['time'].toString(), style: TextStyle(fontFamily: 'BeVietNam')),
+                    ]),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ), onPressed: (){
+          if (GlobalController.i.alertList.elementAt(index)['unread'] == 'true') {
+            GlobalController.i.alertList.elementAt(index)['unread'] = 'false';
+            controller.update(['loadingState']);
+          }
+          if (GlobalController.i.alertList.elementAt(index)['link'].contains('/u/', 0) == true) {
+            Get.toNamed(Routes.Profile, arguments: [GlobalController.i.alertList.elementAt(index)['link']], preventDuplicates: false);
+          } else if (GlobalController.i.alertList.elementAt(index)['link'].contains('/profile-posts/', 0) == true) {
+            // Go to status
+            print('status');
+          } else {
+            Get.toNamed(Routes.View, arguments: [
+              GlobalController.i.alertList.elementAt(index)['title'],
+              GlobalController.i.alertList.elementAt(index)['link'],
+              GlobalController.i.alertList.elementAt(index)['prefixTitle'],
+              GlobalController.i.alertList.elementAt(index)['link'].toString().contains('conversations/messages') ? 1 : 0
+            ]);
+          }
+        }, padding: EdgeInsets.zero,);
+      });
 
+  Widget loading() => Stack(
+        children: [
+          GetBuilder<AlertsController>(
+              id: 'download',
+              builder: (controller) {
+                return LinearProgressIndicator(
+                  value: controller.percentDownload,
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF0CF301)),
+                  backgroundColor: Get.theme.backgroundColor,
+                );
+              }),
+        ],
+      );
 }
--- lib/Page/Alerts/AlertsController.dart
+++ lib/Page/Alerts/AlertsController.dart
@@ -1,10 +1,14 @@
 import 'package:dio_http/dio_http.dart';
 import 'package:get/get.dart';
 import '/GlobalController.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart' as parser;
 
 class AlertsController extends GetxController {
   var dio = Dio(), percentDownload = 0.0;
   Map<String, dynamic> data = {};
+
+  late dom.Document value;
 
   refreshList() async {
     GlobalController.i.alertList.clear();
@@ -35,100 +39,112 @@ class AlertsController extends GetxController {
     }
   }
 
-  String keyStatus(String status) {
-    return status.contains('the thread')
-        ? 'the thread'
-        : status.contains('a thread called')
-            ? 'a thread called'
-            : status.contains('reacted to your message in the conversation') // status
-                ? 'reacted to your message in the conversation'
-                : status.contains('reacted to your')
-                    ? 'reacted to your'
-                    : 'the conversation';
-  }
-
   getAlert() async {
     if (GlobalController.i.isLogged == true) {
       await GlobalController.i.getBodyBeta((value) async {
-        if (value == 1){
-          print('retry');
+        if (value == 1) {
           await getAlert();
         }
       }, (download) {
         percentDownload = download;
         update(['download'], true);
-      }, dio, GlobalController.i.url + '/account/alerts?page=1', false).then((value) {
-        GlobalController.i.token = value!.getElementsByTagName('html')[0].attributes['data-csrf'];
-        if (value.getElementsByTagName('html')[0].attributes['data-logged-in'] == 'true') {
-          GlobalController.i.controlNotification(
-              int.parse(value.getElementsByClassName('p-navgroup-link--alerts')[0].attributes['data-badge'].toString()),
-              int.parse(value.getElementsByClassName('p-navgroup-link--conversations')[0].attributes['data-badge'].toString()),
-              value.getElementsByTagName('html')[0].attributes['data-logged-in'].toString());
-        } else
-          GlobalController.i.controlNotification(0, 0, 'false');
-
-        value.getElementsByClassName('alert js-alert block-row block-row--separated').forEach((element) {
-          data['unread'] = element.attributes['class']!.contains('is-unread') ? 'true' : 'false';
-          data['time'] = element.getElementsByTagName('time')[0].innerHtml;
-          data['status'] = element.getElementsByClassName('contentRow-main contentRow-main--close')[0].text.replaceAll(data['time'], '').trim();
-          data['link'] = element.getElementsByClassName('fauxBlockLink-blockLink')[0].attributes['href'].toString();
-          if (element.getElementsByClassName('username ').length > 0) {
-            data['username'] = element.getElementsByClassName('username ')[0].text;
-            data['status'] = data['status'].replaceAll(data['username'], '');
-            data['key'] = keyStatus(data['status']);
-
-            if (data['status'].contains('is now following you.')) {
-              data['threadName'] = '';
-            } else {
-              data['threadName'] = data['status'].split(data['key'])[1].replaceAll(' There may be more posts after this', '');
-              data['status'] = data['status'].split('.')[0].split(data['key'])[0] + data['key'];
-            }
-          } else {
-            data['threadName'] = data['status'].split(':')[1];
-            data['status'] = data['status'].split(':')[0] + ":";
-            data['username'] = '';
-          }
-
-          if (data['threadName'].contains('with  Ưng')) {
-            data['threadName'] = data['threadName'].split('with  Ưng')[0];
-            data['reaction'] = '1';
-          } else if (data['threadName'].contains('with  Gạch')) {
-            data['threadName'] = data['threadName'].split('with  Gạch')[0];
-            data['reaction'] = '2';
-          } else
-            data['reaction'] = '';
-          if (element.getElementsByTagName('span')[0].attributes['dir'] == 'auto') {
-            data['threadName'] = data['threadName'].replaceFirst(element.getElementsByTagName('span')[0].text, '', 0).trim();
-            data['threadName'] = ' ' + data['threadName'] + ' ';
-            data['prefix'] = element.getElementsByTagName('span')[0].text;
-          } else
-            data['prefix'] = '';
-
-          GlobalController.i.alertList.add({
-            'unread': data['unread'],
-            'username': data['username'],
-            'status': data['prefix'].length > 1 ? data['status'] + ' ' : data['status'],
-            'prefix': data['prefix'],
-            'threadName': data['threadName'],
-            'reaction': data['reaction'],
-            'link': data['link'],
-            'time': data['time'],
-          });
-        });
-        if (GlobalController.i.alertList.length == 0) {
-          GlobalController.i.alertList.add({
-            'unread': 'true',
-            'username': '',
-            'status': 'No Alerts',
-            'threadName': '',
-            'reaction': '',
-            'link': '',
-            'time': '',
-          });
-        }
+      }, dio, GlobalController.i.url + '/account/alerts?page=1', false).then((values) {
+        performQueryHtml(values!);
+        value = values;
       });
-      data['loadingStatus'] = 'done';
-      update(['loadingState']);
     }
+  }
+
+  performQueryHtml(dom.Document value) {
+    GlobalController.i.token = value.getElementsByTagName('html')[0].attributes['data-csrf'];
+    if (value.getElementsByTagName('html')[0].attributes['data-logged-in'] == 'true') {
+      GlobalController.i.controlNotification(
+          int.parse(value.getElementsByClassName('p-navgroup-link--alerts')[0].attributes['data-badge'].toString()),
+          int.parse(value.getElementsByClassName('p-navgroup-link--conversations')[0].attributes['data-badge'].toString()),
+          value.getElementsByTagName('html')[0].attributes['data-logged-in'].toString());
+    } else {
+      GlobalController.i.controlNotification(0, 0, 'false');
+    }
+
+    value.getElementsByClassName('alert js-alert block-row block-row--separated').forEach((element) {
+      data['unread'] = element.attributes['class']!.contains('is-unread') ? 'true' : 'false';
+      data['sentence'] = element.text.trim();
+      data['time'] = element.getElementsByClassName('u-dt')[0].text;
+      data['link'] = element.getElementsByClassName('fauxBlockLink-blockLink')[0].attributes['href'].toString();
+      if (element.getElementsByClassName('username ').length != 0) {
+        data['username'] = element.getElementsByClassName('username ')[0].text + ' ';
+      } else {
+        data['username'] = '';
+      }
+      data['sentence'] = data['sentence'].replaceAll(data['username'], '').replaceAll(data['time'], '').trim();
+      data['title'] = element.getElementsByClassName('fauxBlockLink-blockLink')[0].text;
+      data['phase1'] = data['sentence'].toString().split(data['title'])[0];
+      data['phase3'] = data['sentence'].replaceAll(data['title'], '').split(data['phase1'])[1];
+      data['title'] = data['title'].replaceAll(RegExp('\\s+'), ' ');
+
+      if (data['phase3'].contains('with')) {
+        data['hasReact'] = true;
+        data['react'] = data['phase3'].contains('Ưng') ? '1.png' : '2.png';
+        data['phase3'] = ' with ';
+      } else {
+        data['hasReact'] = false;
+      }
+
+      if (element.getElementsByClassName('label').length != 0) {
+        data['prefixTitle'] = element.getElementsByClassName('label')[0].text;
+        data['title'] = data['title'].replaceAll(data['prefixTitle'], '');
+      } else {
+        data['prefixTitle'] = '';
+      }
+
+        if (data['title'] == 'your post'){
+          data['sentence'].split(data['title'])[1].contains('thread');
+          data['title2'] = data['title'] + ' ';
+          data['phase2'] = ' in the thread ';
+          data['title'] = ' '+data['sentence'].split('thread')[1].replaceAll(data['prefixTitle'],'').trim();
+          if (data['title'].contains('with')){
+            data['title'] = ' '+data['title'].split('with')[0].trim();
+          }
+        } else {
+          data['title2'] = '';
+          data['phase2'] = '';
+        }
+        print(data['username'] + data['phase1'] + data['title2'] + data['phase2'] + data['prefixTitle'] + data['title'] + data['phase3'] + (data['hasReact'] == true ? data['react'] : ''));
+
+
+      var avatar = element.getElementsByClassName('avatar avatar--xxs').first;
+      if (avatar.getElementsByTagName('img').length > 0) {
+        data['avatarLink'] = avatar.getElementsByTagName('img')[0].attributes['src'].toString();
+        if (data['avatarLink'].contains('https') == false) {
+          data['avatarLink'] = GlobalController.i.url + data['avatarLink'];
+        }
+        data['avatarColor1'] = '0x00000000';
+        data['avatarColor2'] = '0x00000000';
+      } else {
+        data['avatarLink'] = 'no';
+        data['avatarColor1'] = '0xFFF' + avatar.attributes['style'].toString().split('#')[1].split(';')[0];
+        data['avatarColor2'] = '0xFFF' + avatar.attributes['style'].toString().split('#')[2];
+      }
+
+      GlobalController.i.alertList.add({
+        'username' : data['username'],
+        'phase1' : data['phase1'],
+        'title2' : data['title2'],
+        'phase2' : data['phase2'],
+        'prefixTitle' : data['prefixTitle'],
+        'title' : data['title'],
+        'phase3' : data['phase3'],
+        'hasReact' : data['hasReact'],
+        'react' : data['react'],
+        'time' : '\n'+data['time'],
+        'unread' : data['unread'],
+        'avatarLink' : data['avatarLink'],
+        'avatarColor1' : data['avatarColor1'],
+        'avatarColor2' : data['avatarColor2'],
+        'link' : data['link'],
+      });
+    });
+    data['loadingStatus'] = 'done';
+    update(['loadingState']);
   }
 }
